b4a0a622f8f24e03126894d9817b07f2c4c8e230
ajojeda <ajojeda@me.com>
2025-04-16 10:04:11 -0700

    Add backend/jest.config.auth.js 1744823050694

---
+++ backend/jest.config.auth.js
@@ -0,0 +1,20 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/auth.spec.test.ts'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  setupFilesAfterEnv: [
+    '<rootDir>/src/test/middleware/auth-middleware.setup.ts'
+  ],
+  verbose: true,
+  testTimeout: 10000
+};
